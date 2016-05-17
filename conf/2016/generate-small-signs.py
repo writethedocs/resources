e798913cc0b67bfdb80422e61a7ca6bbbed3b78d
Anthony Johnson <aj@ohess.org>
2016-05-17 15:49:20 -0700

    Add sign and handout-ish signs with configurable yaml input
    Note, need copy for all of this

---
+++ conf/2016/generate-small-signs.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+
+import sys
+import argparse
+import xml
+from xml.etree.ElementTree import parse, register_namespace, Element
+
+import yaml
+
+def edit_svg_text(document, text=None, output=None):
+    if output is None:
+        output = sys.stdout
+    ns = {
+        'svg': 'http://www.w3.org/2000/svg',
+        'inkscape': 'http://www.inkscape.org/namespaces/inkscape',
+        '': 'http://www.w3.org/2000/svg'
+    }
+    xpath = './/svg:flowRoot[@id="sign-text"]'
+    try:
+        for node in document.findall(xpath, ns):
+            for child in node.findall('svg:flowPara', ns):
+                node.remove(child)
+            for line in text.split('\n'):
+                elem = Element('flowPara')
+                elem.text = line
+                node.append(elem)
+    except TypeError:
+        pass
+    document.write(output)
+
+
+if __name__ == '__main__':
+    register_namespace('svg', 'http://www.w3.org/2000/svg')
+    register_namespace('dc', 'http://purl.org/dc/elements/1.1/')
+    register_namespace('cc', 'http://creativecommons.org/ns#')
+    register_namespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+    register_namespace('xlink', 'http://www.w3.org/1999/xlink')
+    register_namespace('sodipodi', 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd')
+    register_namespace('inkscape', 'http://www.inkscape.org/namespaces/inkscape')
+    register_namespace('', 'http://www.w3.org/2000/svg')
+
+    signs = yaml.load(open('signs.yaml'))
+
+    for (sign_name, sign) in signs.items():
+        filename = sign.get('file', 'sign-small.svg')
+        document = parse(filename)
+        output_filename = filename.replace('.svg', '-{0}.svg'.format(sign_name))
+        h = open(output_filename, 'w')
+        edit_svg_text(document, text=sign.get('text'), output=h)
